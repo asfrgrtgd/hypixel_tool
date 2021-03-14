bb36a9b0e104ea7d932eabc7952135da9e41eebd
Namu333 <ar2556441@gmail.com>
2021-03-14 18:11:45 +0900

    FIX

--- src/main/java/jp/cactuses/hypixelhighqolmod/hypixelhighqolmod.java
+++ src/main/java/jp/cactuses/hypixelhighqolmod/hypixelhighqolmod.java
@@ -1,14 +1,14 @@
-package com.example.examplemod;
+package com.example.hypixelhighqolmod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = hypixelhighqolmod.MODID, version = hypixelhighqolmod.VERSION)
+public class hypixelhighqolmod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "hypixelhighqolmod";
     public static final String VERSION = "1.0";
     
     @EventHandler
