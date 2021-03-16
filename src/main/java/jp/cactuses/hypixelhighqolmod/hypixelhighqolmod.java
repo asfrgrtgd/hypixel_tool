ff7a82556a4a3cb9a68a585ff4e4ca9e3e1efd77
kang <61167784+kang-jp@users.noreply.github.com>
2021-03-16 18:34:28 +0900

    create branch

--- src/main/java/jp/cactuses/hypixelhighqolmod/hypixelhighqolmod.java
+++ src/main/java/jp/cactuses/hypixelhighqolmod/hypixelhighqolmod.java
@@ -1,4 +1,4 @@
-package com.example.hypixelhighqolmod;
+package jp.cactuses.hypixelhighqolmod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -6,15 +6,21 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = hypixelhighqolmod.MODID, version = hypixelhighqolmod.VERSION)
-public class hypixelhighqolmod
-{
+public class hypixelhighqolmod{
     public static final String MODID = "hypixelhighqolmod";
     public static final String VERSION = "1.0";
-    
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    public void init(FMLInitializationEvent event){
+        FMLCommonHandler.instance().bus().register(this);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onChat(ClientChatReceivedEvent event){
+    String message = event.message.getUnformattedText();
+        if(message.contains("hi")){
+            Minecraft.getMinecraft().toggleFullscreen();
+        }
     }
 }
