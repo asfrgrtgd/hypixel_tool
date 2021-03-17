50906e18384cc335a98fe449fcbfeee3403fcb4c
Namu333 <ar2556441@gmail.com>
2021-03-17 18:25:59 +0900

    wipe trash

--- src/main/java/jp/cactuses/hypixelhighqoltool/hypixelhighqoltool.java
+++ src/main/java/jp/cactuses/hypixelhighqoltool/hypixelhighqoltool.java
@@ -4,9 +4,11 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.event.CommandEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -21,7 +23,7 @@ public class hypixelhighqoltool{
         FMLCommonHandler.instance().bus().register(this);
         MinecraftForge.EVENT_BUS.register(this);
     }
-
+    
     @SubscribeEvent
     public void onChat(ClientChatReceivedEvent event){
     String message = event.message.getUnformattedText();
@@ -29,4 +31,5 @@ public class hypixelhighqoltool{
             Minecraft.getMinecraft().toggleFullscreen();
         }
     }
+
 }
