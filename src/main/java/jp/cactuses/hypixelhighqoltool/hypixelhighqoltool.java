81057de2146f03e659e5ab76ec528964231e7367
Namu333 <ar2556441@gmail.com>
2021-03-18 19:18:20 +0900

    /p listを発言するまで完成(.rpを打ってもchateventがキャンセルされてログに残らない仕様)

--- src/main/java/jp/cactuses/hypixelhighqoltool/hypixelhighqoltool.java
+++ src/main/java/jp/cactuses/hypixelhighqoltool/hypixelhighqoltool.java
@@ -1,5 +1,8 @@
 package jp.cactuses.hypixelhighqoltool;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -12,11 +15,17 @@ import net.minecraftforge.event.CommandEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
+import net.minecraftforge.event.CommandEvent;
+import net.minecraftforge.event.ServerChatEvent;
+import net.minecraft.client.gui.GuiChat;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiContainer;
 @Mod(modid = hypixelhighqoltool.MODID, version = hypixelhighqoltool.VERSION)
 public class hypixelhighqoltool{
     public static final String MODID = "hypixelhighqoltool";
     public static final String VERSION = "1.0";
+    public String test = "FIRST";
 
     @EventHandler
     public void init(FMLInitializationEvent event){
@@ -25,11 +34,23 @@ public class hypixelhighqoltool{
     }
     
     @SubscribeEvent
-    public void onChat(ClientChatReceivedEvent event){
-    String message = event.message.getUnformattedText();
-        if(message.contains("hi")){
-            Minecraft.getMinecraft().toggleFullscreen();
+    public void receivecommand(ServerChatEvent event){
+        String com = event.message;
+        test = com;
+        if(com.equals(".rp")){
+            test = "i got that";
+            Minecraft.getMinecraft().thePlayer.sendChatMessage("/p list");
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public void onRender(RenderTickEvent event){
+        if(Minecraft.getMinecraft().inGameHasFocus){
+            Minecraft.getMinecraft().fontRendererObj.drawString(test,300,5,5312237);
         }
     }
 
 }
+
+    
